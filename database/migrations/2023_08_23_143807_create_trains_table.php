352383f8cc6a3dd834020b57ba2a5951b5934fcb
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-08-23 18:02:41 +0200

    train seeder created

--- database/migrations/2023_08_23_143807_create_trains_table.php
+++ database/migrations/2023_08_23_143807_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->integer('number_of_carriages');
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
             $table->timestamps();
         });
     }
